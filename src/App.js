1c578d1e66e17512f2cc166bef476228b012b93a
Sinthu <kumarasamy.s@husky.neu.edu>
2020-06-02 14:44:11 -0400

    Added functionality to predict winner

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   const [matrix,setMatrix] = useState(initialState);
   const [matrixSize,setMatrixSize] = useState(3);
   const [currentPlayer,setCurrentPlayer] = useState("o");
+  const [selectedRow,setSelectedRow] = useState(null);
+  const [selectedCol,setSelectedCol] = useState(null);
+  const [winner,setWinner] = useState(false);
   useEffect(()=>{
     const row = new Array(matrixSize).fill(null);
     const tempMatrix = [];
@@ -17,7 +20,9 @@ function App() {
   },[])
 
   function onClickHandle(row,col){
-    if(!matrix[row][col]){
+    if(!matrix[row][col] && !winner){
+      setSelectedCol(col)
+      setSelectedRow(row)
       const nextPlayer = currentPlayer === 'x' ? 'o' : 'x';
       setCurrentPlayer(nextPlayer)
       const updatedMatrix = [...matrix]
@@ -26,6 +31,34 @@ function App() {
     }
 
   }
+  function isWinner(){
+    let vertical=true;
+    let horizontal = true;
+    let diagonal1 = true;
+    let diagonal2 = true;
+    if(selectedCol === null || selectedRow === null){ return}
+    for(let i=0;i<matrixSize;i++){
+      if(matrix[i][selectedCol]!==currentPlayer){
+        vertical = false;
+      }
+      if(matrix[selectedRow][i]!==currentPlayer){
+        horizontal = false;
+      }
+      if(matrix[i][i]!==currentPlayer){
+        diagonal1 = false;
+      }
+      if(matrix[i][i-matrixSize-1]!==currentPlayer){
+        diagonal2 = false;
+      }
+
+    }
+    if(horizontal || vertical || diagonal1 || diagonal2){setWinner(true)}
+  }
+  useEffect(()=>{
+    if(!winner){
+      isWinner();
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -41,6 +74,7 @@ function App() {
           </div>
          ))
        }
+       <h2>{winner ? `Player ${currentPlayer} is a winner`:''}</h2>
       </header>
     </div>
   );
